c6c9f30eb23963a5666e3fc76cdeb3bab4e5dd3e
Himanshu <himanshu.a.04.1999@gmail.com>
2025-05-25 20:56:15 -0400

    Add transation functionality

--- lib/pages/mpay_home.dart
+++ lib/pages/mpay_home.dart
@@ -16,175 +16,182 @@ class _MpayHomeState extends State<MpayHome> {
   final String? currentUserId = FirebaseAuth.instance.currentUser?.uid;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Mpay Home')),
-      body: Center(
-        child: Column(
-          children: [
-            if (currentUserId !=
-                null) // Ensured user is logged in - Added this condition
-              StreamBuilder(
-                // Added this widget
-                stream:
-                    FirebaseDatabase.instance
-                        .ref('games')
-                        .child(widget.gameId)
-                        .child('Players')
-                        .child(
-                          currentUserId!,
-                        ) // Targeting the specific player - Added this line
-                        .onValue,
-                builder: (
-                  BuildContext context,
-                  AsyncSnapshot<DatabaseEvent> snapshot,
-                ) {
-                  if (snapshot.hasData) {
-                    final playerData =
-                        snapshot.data!.snapshot.value
-                            as Map<dynamic, dynamic>?; // Modified this line
-                    if (playerData != null) {
-                      final playerName =
-                          playerData['name'] ?? 'No Name'; // Modified this line
-                      final playerBalance =
-                          playerData['balance'] ?? '0'; // Modified this line
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+        appBar: AppBar(title: Text('Mpay Home')),
+        body: Center(
+          child: Column(
+            children: [
+              if (currentUserId !=
+                  null) // Ensured user is logged in - Added this condition
+                StreamBuilder(
+                  // Added this widget
+                  stream:
+                      FirebaseDatabase.instance
+                          .ref('games')
+                          .child(widget.gameId)
+                          .child('Players')
+                          .child(
+                            currentUserId!,
+                          ) // Targeting the specific player - Added this line
+                          .onValue,
+                  builder: (
+                    BuildContext context,
+                    AsyncSnapshot<DatabaseEvent> snapshot,
+                  ) {
+                    if (snapshot.hasData) {
+                      final playerData =
+                          snapshot.data!.snapshot.value
+                              as Map<dynamic, dynamic>?; // Modified this line
+                      if (playerData != null) {
+                        final playerName =
+                            playerData['name'] ??
+                            'No Name'; // Modified this line
+                        final playerBalance =
+                            playerData['balance'] ?? '0'; // Modified this line
 
-                      return Card(
-                        // Added this widget
-                        margin: const EdgeInsets.all(16.0),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 16,
-                            horizontal: 40,
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: <Widget>[
-                              Text(
-                                '$playerName',
-                                style: const TextStyle(
-                                  fontSize: 32,
-                                  fontWeight: FontWeight.w700,
+                        return Card(
+                          // Added this widget
+                          margin: const EdgeInsets.all(16.0),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 16,
+                              horizontal: 40,
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                Text(
+                                  '$playerName',
+                                  style: const TextStyle(
+                                    fontSize: 32,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                  textAlign: TextAlign.center,
                                 ),
-                                textAlign: TextAlign.center,
-                              ),
-                              SizedBox(height: 10),
-                              Text('Your MPay Balance'),
-                              Text(
-                                '$playerBalance',
-                                style: const TextStyle(
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                                textAlign: TextAlign.center,
-                              ), // Modified this line
-                            ],
-                          ),
-                        ),
-                      );
-                    }
-                  } else if (snapshot.hasError) {
-                    return Padding(
-                      // Added this widget
-                      padding: const EdgeInsets.all(16.0),
-                      child: Text(
-                        'Error loading your details: ${snapshot.error}',
-                        style: const TextStyle(color: Colors.red),
-                      ), // Added this line
-                    );
-                  } else {
-                    return const Padding(
-                      // Added this widget
-                      padding: EdgeInsets.all(16.0),
-                      child: Text('Loading your details...'), // Added this line
-                    );
-                  }
-                  return const SizedBox.shrink(); // Fallback if data is null - Added this line
-                },
-              ),
-            SizedBox(height: 20),
-            Text('Pay Other Players'),
-            SizedBox(height: 20),
-            Expanded(
-              child: StreamBuilder(
-                stream:
-                    FirebaseDatabase.instance
-                        .ref('games')
-                        .child(widget.gameId)
-                        .child('Players')
-                        .onValue,
-                builder: (
-                  BuildContext context,
-                  AsyncSnapshot<DatabaseEvent> snapshot,
-                ) {
-                  if (snapshot.hasData) {
-                    final playersData =
-                        snapshot.data!.snapshot.value as Map<dynamic, dynamic>?;
-                    if (playersData == null || playersData.isEmpty) {
-                      return const Center(
-                        child: Text('No other players in this game yet.'),
-                      );
-                    }
-
-                    final playerIds =
-                        playersData.keys.toList(); // Get all player IDs
-                    final currentPlayerId =
-                        FirebaseAuth.instance.currentUser?.uid;
-                    final otherPlayerIds =
-                        playersData.keys
-                            .where((playerId) => playerId != currentPlayerId)
-                            .toList();
-
-                    return GridView.builder(
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 4,
-                          ),
-                      itemCount: otherPlayerIds.length,
-                      itemBuilder: (context, index) {
-                        final playerId = otherPlayerIds[index];
-                        final player =
-                            playersData[playerId] as Map<dynamic, dynamic>?;
-                        final playerName = player?['name'] ?? 'No Name';
-
-                        return InkWell(
-                          onTap: () {
-                            if (mounted) {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder:
-                                      (context) => PayScreen(
-                                        gameId: widget.gameId,
-                                        fromPlayerId: currentPlayerId!,
-                                        toPlayerId: playerId,
-                                      ),
-                                ),
-                              );
-                            }
-                          },
-                          child: Card(
-                            margin: const EdgeInsets.all(8.0),
-                            child: Center(
-                              child: Text(
-                                playerName,
-                                textAlign: TextAlign.center,
-                              ),
+                                SizedBox(height: 10),
+                                Text('Your MPay Balance'),
+                                Text(
+                                  '$playerBalance',
+                                  style: const TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ), // Modified this line
+                              ],
                             ),
                           ),
                         );
-                      },
-                    );
-                  } else if (snapshot.hasError) {
-                    return Center(
-                      child: Text('Error loading players: ${snapshot.error}'),
-                    );
-                  } else {
-                    return const Center(child: CircularProgressIndicator());
-                  }
-                },
+                      }
+                    } else if (snapshot.hasError) {
+                      return Padding(
+                        // Added this widget
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text(
+                          'Error loading your details: ${snapshot.error}',
+                          style: const TextStyle(color: Colors.red),
+                        ), // Added this line
+                      );
+                    } else {
+                      return const Padding(
+                        // Added this widget
+                        padding: EdgeInsets.all(16.0),
+                        child: Text(
+                          'Loading your details...',
+                        ), // Added this line
+                      );
+                    }
+                    return const SizedBox.shrink(); // Fallback if data is null - Added this line
+                  },
+                ),
+              SizedBox(height: 20),
+              Text('Pay Other Players'),
+              SizedBox(height: 20),
+              Expanded(
+                child: StreamBuilder(
+                  stream:
+                      FirebaseDatabase.instance
+                          .ref('games')
+                          .child(widget.gameId)
+                          .child('Players')
+                          .onValue,
+                  builder: (
+                    BuildContext context,
+                    AsyncSnapshot<DatabaseEvent> snapshot,
+                  ) {
+                    if (snapshot.hasData) {
+                      final playersData =
+                          snapshot.data!.snapshot.value
+                              as Map<dynamic, dynamic>?;
+                      if (playersData == null || playersData.isEmpty) {
+                        return const Center(
+                          child: Text('No other players in this game yet.'),
+                        );
+                      }
+
+                      final playerIds =
+                          playersData.keys.toList(); // Get all player IDs
+                      final currentPlayerId =
+                          FirebaseAuth.instance.currentUser?.uid;
+                      final otherPlayerIds =
+                          playersData.keys
+                              .where((playerId) => playerId != currentPlayerId)
+                              .toList();
+
+                      return GridView.builder(
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 4,
+                            ),
+                        itemCount: otherPlayerIds.length,
+                        itemBuilder: (context, index) {
+                          final playerId = otherPlayerIds[index];
+                          final player =
+                              playersData[playerId] as Map<dynamic, dynamic>?;
+                          final playerName = player?['name'] ?? 'No Name';
+
+                          return InkWell(
+                            onTap: () {
+                              if (mounted) {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder:
+                                        (context) => PayScreen(
+                                          gameId: widget.gameId,
+                                          fromPlayerId: currentPlayerId!,
+                                          toPlayerId: playerId,
+                                        ),
+                                  ),
+                                );
+                              }
+                            },
+                            child: Card(
+                              margin: const EdgeInsets.all(8.0),
+                              child: Center(
+                                child: Text(
+                                  playerName,
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    } else if (snapshot.hasError) {
+                      return Center(
+                        child: Text('Error loading players: ${snapshot.error}'),
+                      );
+                    } else {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/pages/pay_screen.dart
+++ lib/pages/pay_screen.dart
@@ -40,12 +40,139 @@ class _PayScreenState extends State<PayScreen> {
         recipientName =
             playerData?[widget.gameId]['Players'][widget.toPlayerId]['name'];
       });
-      print(recipientName);
     } else {
       setState(() {
         recipientName = 'Recipient not found';
       });
     }
+  }
+
+  Future<void> _submitPayment() async {
+    final String amountText = _amount.text.trim();
+    if (amountText.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Please enter an amount to pay.')),
+      );
+      return; // Exit the method if no amount is entered
+    }
+
+    final double? amountToPay = double.tryParse(
+      amountText,
+    ); // Convert text to a number
+
+    if (amountToPay == null || amountToPay <= 0) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Please enter a valid amount greater than 0.'),
+        ),
+      );
+      return; // Exit if the amount is not valid
+    }
+
+    final String fromPlayerId = widget.fromPlayerId;
+
+    final DatabaseReference senderRef = FirebaseDatabase.instance
+        .ref('games')
+        .child(widget.gameId)
+        .child('Players')
+        .child(fromPlayerId);
+
+    final snapshot = await senderRef.get();
+    if (!snapshot.exists || snapshot.value == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Could not retrieve your current balance.'),
+        ),
+      );
+      return;
+    }
+
+    final gameData =
+        snapshot.value
+            as Map<dynamic, dynamic>?; // Rename to gameData to reflect content
+
+    final playersData =
+        gameData?[widget.gameId]?['Players']
+            as Map<dynamic, dynamic>?; // Access the Players node
+    final currentPlayer =
+        playersData?[fromPlayerId]
+            as Map<dynamic, dynamic>?; // Access the current player's data
+    final currentBalance = currentPlayer?['balance'] ?? 0;
+
+    if (amountToPay > currentBalance) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text('Insufficient balance.')));
+      return; // Exit if balance is insufficient
+    }
+
+    //process payment
+
+    final String toPlayerId = widget.toPlayerId;
+    final double newSenderBalance = currentBalance - amountToPay;
+
+    final DatabaseReference receiverRef = FirebaseDatabase.instance
+        .ref('games')
+        .child(widget.gameId)
+        .child('Players')
+        .child(toPlayerId);
+
+    final receiverSnapshot = await receiverRef.get();
+    if (!receiverSnapshot.exists || receiverSnapshot.value == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Could not retrieve recipient\'s current balance.'),
+        ),
+      );
+      return;
+    }
+
+    final receiverPlayerData = receiverSnapshot.value as Map<dynamic, dynamic>?;
+    final currentRecipientBalance =
+        receiverPlayerData?[widget.gameId]['Players'][widget
+                .toPlayerId]['balance']
+            as int? ??
+        0;
+
+    final double newRecipientBalance = currentRecipientBalance + amountToPay;
+    print(newRecipientBalance);
+    print(newSenderBalance);
+
+    // actual payment
+
+    final DatabaseReference receiverReference = FirebaseDatabase.instance
+        .ref('games')
+        .child(
+          widget.gameId,
+        ) // Access the gameId passed from the previous screen
+        .child('Players')
+        .child(widget.toPlayerId);
+
+    await receiverReference.set({
+      'name':
+          receiverPlayerData?[widget.gameId]['Players'][widget
+              .toPlayerId]['name'],
+      'balance': newRecipientBalance,
+      'isAdmin':
+          receiverPlayerData?[widget.gameId]['Players'][widget
+              .toPlayerId]['isAdmin'],
+    });
+
+    //sender balance
+
+    final DatabaseReference senderReference = FirebaseDatabase.instance
+        .ref('games')
+        .child(
+          widget.gameId,
+        ) // Access the gameId passed from the previous screen
+        .child('Players')
+        .child(widget.fromPlayerId);
+
+    await senderReference.set({
+      'name': currentPlayer?['name'],
+      'balance': newSenderBalance,
+      'isAdmin': currentPlayer?['isAdmin'],
+    });
   }
 
   @override
@@ -69,7 +196,7 @@ class _PayScreenState extends State<PayScreen> {
               ElevatedButton(
                 // Add this button
                 onPressed: () {
-                  // _submitPayment();
+                  _submitPayment();
                 },
                 child: const Text('Confirm Payment'),
               ),
